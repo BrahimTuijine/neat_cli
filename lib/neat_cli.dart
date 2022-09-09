2336cfd2231f937b9046858e0718eec88a78b1c2
Merseni Bilel <bilelmerseni7016@gmail.com>
2022-09-09 16:31:20 +0100

    🚀🚀 init the neat cli 🧼

---
+++ lib/neat_cli.dart
@@ -0,0 +1,9 @@
+/// neat_cli : CREATE FLUTTER CLEAN ARCHITECTURE PROJECTS
+/// ```sh
+/// # activate neat_cli
+/// dart pub global activate neat_cli
+///
+/// # see usage
+/// neat_cli --help
+/// ```
+library neat_cli;
